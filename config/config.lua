fc5c60082e7d90ecddfbe7407cfcbd63dbc2c3ca
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-10-27 10:53:22 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,21 +1,25 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
 config = {
-    -- Departments that has access to the police mdt.
+    -- Jobs that has access to the police version mdt.
     policeAccess = {
-        ["SAHP"] = true,
-        ["LSPD"] = true,
-        ["BCSO"] = true
+        ["sahp"] = true,
+        ["lspd"] = true,
+        ["bcso"] = true
     },
 
-    -- Departments that has access to the fire mdt.
+    -- Jobs that has access to the fire version mdt.
     fireAccess = {
-        ["LSFD"] = true
+        ["lsfd"] = true
+    },
+
+    -- Jobs that has access to the tow version mdt.
+    towAccess = {
+        ["tow"] = true
     },
 
     -- Set to true if you'd like to use a postal script with 911 calls.
     use911Postal = true,
-    postalResourceName = "nearest-postal", -- name of nearest postal script (make sure to have one installed).
-
+    postalResourceName = "nearest-postal" -- name of nearest postal script (make sure to have one installed).
 
 }
