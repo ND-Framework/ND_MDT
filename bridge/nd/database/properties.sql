373f67773c77fcd413b8f255e8bbacd47f2650b6
TabH2 <149743341+TabH2@users.noreply.github.com>
2024-01-28 00:37:30 -0600

    Delete bridge/nd/database/properties.sql

--- bridge/nd/database/properties.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `nd_properties` (
-    `id` VARCHAR(250) DEFAULT NULL,
-    `owner` INT(11) DEFAULT NULL,
-    `access` LONGTEXT DEFAULT '[]',
-    `address` VARCHAR(250) DEFAULT NULL,
-    `sale` INT(11) DEFAULT NULL,
-    INDEX `owner` (`owner`) USING BTREE,
-    CONSTRAINT `property_owner` FOREIGN KEY (`owner`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
-);
