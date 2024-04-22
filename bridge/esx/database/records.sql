ecf98bad55e08daab1d42ae9e67e2ff31a0c83d1
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2024-04-22 19:50:57 +0200

    fix(esx-bridge): altered records.sql to work with identifier

--- bridge/esx/database/records.sql
+++ bridge/esx/database/records.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `nd_mdt_records` (
-	`character` INT(11) DEFAULT NULL,
+	`character` VARCHAR(60) DEFAULT NULL,
 	`records` LONGTEXT DEFAULT '[]',
 	INDEX `character` (`character`) USING BTREE,
 	CONSTRAINT `FK_nd_mdt_characters` FOREIGN KEY (`character`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
