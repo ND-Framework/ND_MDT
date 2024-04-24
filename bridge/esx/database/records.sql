b4ca2c17f8faefefdfa17edceec7dc7d52f7feea
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2024-04-24 21:36:33 +0200

    fix: temporary for ESX sql

--- bridge/esx/database/records.sql
+++ bridge/esx/database/records.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `nd_mdt_records` (
 	`character` VARCHAR(60) DEFAULT NULL,
-	`records` LONGTEXT DEFAULT '[]',
+	`records` LONGTEXT DEFAULT '[]'/* ,
 	INDEX `character` (`character`) USING BTREE,
-	CONSTRAINT `FK_nd_mdt_characters` FOREIGN KEY (`character`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT `FK_nd_mdt_characters` FOREIGN KEY (`character`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE */
 );
--- bridge/esx/database/weapons.sql
+++ bridge/esx/database/weapons.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `nd_mdt_weapons` (
 	`weapon` VARCHAR(50) DEFAULT NULL,
 	`serial` VARCHAR(50) DEFAULT NULL,
 	`owner_name` VARCHAR(100) DEFAULT NULL,
-	`stolen` INT(1) DEFAULT '0',
+	`stolen` INT(1) DEFAULT '0'/* ,
 	INDEX `FK_nd_mdt_weapons_characters` (`character`) USING BTREE,
-	CONSTRAINT `FK_nd_mdt_weapons_characters` FOREIGN KEY (`character`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT `FK_nd_mdt_weapons_characters` FOREIGN KEY (`character`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE */
 );
